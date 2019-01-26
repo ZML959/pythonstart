992b8371e04330c1a7f386727b3b3eacac1502c1
zml959 <zml959@foxmail.com>
2019-01-26 18:16:53 +0800

    zml

--- day2/可变参数.py
+++ day2/可变参数.py
@@ -13,4 +13,24 @@ print(calc(*nums))
 def person(name, age, **kw):
   print('name',name,'age',age,'other',kw)
 
-person('Michael',30)
+# person('Michael',30)
+# person('Bob',35,city='Beijing')
+extra = {
+  'city' : 'beijing',
+  'job' : 'engineer'
+}
+# person('Jack',24,city=extra['city'],job=extra['job'])
+
+def person2(name, age, **kw):
+  if 'city' in kw:
+    #有city参数
+    print('我有city')
+    # pass
+  if 'job' in kw:
+    # 有job参数
+    print('我有job')
+    # pass
+  print('name', name, 'age', age, 'other',kw)
+person2('ZML', 25, ciyt = 'city', addr = 'changping', zipcode = 123456 )
+person2('ZML', 25, city = 'city' )
+
---
+++ day3/qiepian.py
@@ -0,0 +1,10 @@
+L = ['michael','sarah','tracy','bobo']
+r = []
+n = 3
+for i in range(n):
+  r.append(L[i]) 
+print(r)
+#range(start, stop[,step])
+# range(10) 从0开始到10
+# range(1,11)从1开始到11
+# range(0,30,5) 步长为5
