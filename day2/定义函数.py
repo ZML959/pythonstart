041efc0200cd22a6c3a5e277f0f8f86afb06dc42
zml959 <zml959@foxmail.com>
2019-01-26 14:14:30 +0800

    zml

--- day2/定义函数.py
+++ day2/定义函数.py
@@ -6,5 +6,25 @@ def my_first(x):
   else:
     return -x
     print('我是小于0')
-my_first(-99)
+
+print(my_first(-99))
 #- - 
+def power(x):
+  return x * x
+a = power
+print(a(99))
+
+
+def power1(x,n):
+  s = 1
+  while n > 0:
+    n = n - 1 
+    s = s * x
+  return s
+print(power1(10,8))
+
+def add_end(L=[]):
+  L.append('END')
+  return L
+
+add_end([1,2,3])
